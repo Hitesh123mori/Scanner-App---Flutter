205b2048f616e49f5dfd393c1092e1ff59b05188
Harsh Rajput <harshmori0521@gmail.com>
2023-04-29 15:28:06 +0530

    1se

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,57 +14,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
       debugShowCheckedModeBanner: false,
 
-      home: DefaultTabController(
-        initialIndex: 1,
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Scanner"),
-            actions: [
-              Padding(
-                padding: const EdgeInsets.only(right: 28.0),
-                child: IconButton(
-                  icon : Icon(Icons.search),
-                  onPressed: (){},
-                ),
-              ) ,
-              Padding(
-                padding: const EdgeInsets.only(right: 18.0),
-                child: IconButton(
-                  icon: Icon(Icons.settings),
-                  onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>Setting())) ;
-                  },
+      home: Scaffold(
 
-                ),
-              )
-            ],
-            bottom: TabBar(
-              isScrollable: true,
-              tabs: [
-                Container(
-                  width: MediaQuery.of(context).size.width*0.42,
-                  child: Tab(
-                    text: "Leads",
-                  ),
-                ),
-
-                Container(
-                  width: MediaQuery.of(context).size.width*0.42,
-                  child: Tab(
-                    text: "Generate QR Code",
-                  ),
-                )
-              ] ,
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              LeadsTabScreen() ,
-              GenerateQRTabscreen(),
-            ],
-          ),
-        ),
       ),
     ) ;
   }
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hackathon_scanner_app/user.dart' as user ;
 
-import 'login.dart';
+import 'login_screen.dart';
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
