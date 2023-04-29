91d3d82c6bb91ccb7913b475e8fb4b0b0fc9d786
Harsh Rajput <harshmori0521@gmail.com>
2023-04-29 19:07:14 +0530

    finall commmit

--- lib/row_material/setting.dart
+++ lib/row_material/setting.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart' ;
 
 import '../screens/home.dart';
+import 'Profile.dart';
 
 class bussinesInfo extends StatefulWidget {
   const bussinesInfo({Key? key}) : super(key: key);
@@ -31,13 +32,13 @@ class _bussinesInfoState extends State<bussinesInfo> {
                 Container(
                   width: MediaQuery.of(context).size.width*0.42,
                   child: Tab(
-                    text: "Leads",
+                    text: "Business",
                   ),
                 ),
                 Container(
                   width: MediaQuery.of(context).size.width*0.42,
                   child: Tab(
-                    text: "Leads",
+                    text: "Auto Respond Text",
                   ),
                 ),
 
@@ -46,6 +47,12 @@ class _bussinesInfoState extends State<bussinesInfo> {
             ),
             title: Text("Setting"),
           ),
+          body: TabBarView(
+            children: [
+              Profile() ,
+
+        ],
+      ),
         ),
 
       ),
