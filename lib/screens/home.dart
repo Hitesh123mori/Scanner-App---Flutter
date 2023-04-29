ce24656a69df6d472b20c54b32555fbac8cc975e
Niraj kc <niraj.kc.128@gmail.com>
2023-04-29 19:05:42 +0530

    QR code Scanner Added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart' ;
 import 'package:hackathon_scanner_app/widgets/app_colors.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../row_material/setting.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
+
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,6 +15,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  Future _scanQRCode() async {
+    var cameraStatus = await Permission.camera.status;
+    if(cameraStatus.isGranted){
+      String? qrData = await scanner.scan();
+      print(qrData);
+    }
+    else{
+      var isgrant = await Permission.camera.request();
+      if (isgrant.isGranted){
+        String? qrData = await scanner.scan();
+        print(qrData);
+      }
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -55,7 +76,7 @@ class _HomeScreenState extends State<HomeScreen> {
             Padding(
               padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.75,left: MediaQuery.of(context).size.width*0.75),
               child: FloatingActionButton(
-                  onPressed: (){},
+                  onPressed: (){_scanQRCode();},
                   child:Icon(Icons.document_scanner_outlined,) ,
               ),
             )
